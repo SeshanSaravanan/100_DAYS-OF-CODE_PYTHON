8300e7a8aa3804312a001a1ef5b5a745aa5ed250
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-05-01 18:19:50 +0530

    Create ex_2.py

---
+++ Day_1/Coding Exercises/ex_2.py
@@ -0,0 +1,26 @@
+DEBUGGING
+
+/* Instructions
+Look at the code in the code editor on the left. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+Warning: The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+
+Example Output
+When you run your program, it should print the following:
+
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n. */ 
+
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
