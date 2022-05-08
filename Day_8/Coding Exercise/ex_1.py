580745e516b2cff981818ad599e15583a643f4a1
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-05-08 10:02:13 +0530

    Update ex_1.py

--- Day_8/Coding Exercise/ex_1.py
+++ Day_8/Coding Exercise/ex_1.py
@@ -1,3 +1,5 @@
+                                        PAINT AREA CALCULATOR
+
 Task: Instructions
 You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
 
