fa6f1ceb76bfb664845c98899694eca7498d6476
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-13 15:24:21 +0530

    Create main.py

---
+++ Day _012/CODING EXERCISES/1. NAMESPACES/main.py
@@ -0,0 +1,12 @@
+################### Scope ####################
+
+#global scope 
+enemies = 1
+
+def increase_enemies():
+  #local scope
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
