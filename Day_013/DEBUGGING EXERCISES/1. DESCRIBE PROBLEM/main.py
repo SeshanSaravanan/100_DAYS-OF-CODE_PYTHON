0dd7f456c2b73622e87dfe138223e7155b5d051a
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-15 19:31:59 +0530

    Create main.py

---
+++ Day_013/DEBUGGING EXERCISES/1. DESCRIBE PROBLEM/main.py
@@ -0,0 +1,7 @@
+# Describe Problem -- Solution 
+
+def my_function():
+  for i in range(1, 21):  # The upper bound of the range function is debugged 
+    if i == 20:
+      print("You got it")
+my_function()
