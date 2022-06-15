ae6485dcfeaf9e0da3c2de58e0836c3d61f72904
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-15 19:41:03 +0530

    Create main.py

---
+++ Day_013/DEBUGGING EXERCISES/2.  REPRODUCE THE BUG/main.py
@@ -0,0 +1,7 @@
+# # Reproduce the Bug -- Solution
+
+
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)  # here the index in debugged 
+print(dice_imgs[dice_num])
