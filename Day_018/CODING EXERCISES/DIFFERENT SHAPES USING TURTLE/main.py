710bc92e5ed54c7ac0c399480cdfd17c969221cb
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-17 14:43:00 +0530

    Update main.py

--- Day_018/CODING EXERCISES/DIFFERENT SHAPES USING TURTLE/main.py
+++ Day_018/CODING EXERCISES/DIFFERENT SHAPES USING TURTLE/main.py
@@ -5,11 +5,14 @@ import random
 tim = t.Turtle()
 
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "SlateGrey", "SeaGreen"]
+
+
 def draw_shape(num_sides):
     angle = 360 / num_sides
     for _ in range(num_sides):
         tim.forward(100)
         tim.right(angle)
+
 
 for shape_side_n in range(3, 10):
     tim.color(random.choice(colors))
