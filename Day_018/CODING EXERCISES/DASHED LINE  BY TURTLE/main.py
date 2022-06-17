094874316d6c8b830031a16957aa56f557b73b5e
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-06-17 13:12:46 +0530

    Add files via upload

---
+++ Day_018/CODING EXERCISES/DASHED LINE  BY TURTLE/main.py
@@ -0,0 +1,13 @@
+# Drawing a square shape
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.down()
+
+screen = Screen()
+screen.exitonclick()
